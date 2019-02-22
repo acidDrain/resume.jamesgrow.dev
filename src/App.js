56bd64f8ad8a21adcbee8ba3c6cf07b77443291f
acidDrain <jmgrow@gmail.com>
2019-02-22 00:40:29 -0600

    Finished directory scaffolding and updated App.js/App.css for Grid/with content

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 
 import Header from "./header/Header";
 import Blurb from "./blurb/Blurb";
+import LearnNext from "./learn-next/LearnNext";
+import OpenSource from "./open-source/OpenSource";
+import Programming from "./programming/Programming";
+import SideProjects from "./side-projects/SideProjects";
+import Systems from "./systems/Systems";
+import VendorTech from "./vendor-tech/VendorTech";
+import WorkHistory from "./work-history/WorkHistory";
 
 import "./App.css";
 
@@ -9,12 +16,28 @@ import data from "./data.json";
 
 class App extends Component {
   render() {
+    const vendorTech = data.resume_data.sections.vendorTech;
+    const workHistory = data.resume_data.sections.workHistory;
+    const keySkills = data.resume_data.sections.keySkills;
+    const interestingProjects = data.resume_data.sections.interestingProjects;
+    const programming = data.resume_data.sections.programming;
+    const systems = data.resume_data.sections.systems;
+    const openSource = data.resume_data.sections.openSource;
+    const sideProjects = data.resume_data.sections.sideProjects;
+    const learnNext = data.resume_data.sections.learnNext;
     const contactInfo = data.resume_data.sections.contactInfo;
     const blurb = data.resume_data.sections.blurb;
     return (
       <div className="App">
         <Header {...contactInfo} />
         <Blurb {...blurb} />
+        <LearnNext {...learnNext} />
+        <OpenSource {...openSource} />
+        <Programming {...programming} />
+        <SideProjects {...sideProjects} />
+        <Systems {...systems} />
+        <VendorTech {...vendorTech} />
+        <WorkHistory {...workHistory} />
       </div>
     );
   }
