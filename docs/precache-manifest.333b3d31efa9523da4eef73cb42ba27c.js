c864d406e8a5786992db062c36bb35626df9a0db
acidDrain <jmgrow@gmail.com>
2021-12-21 04:41:43 -0600

    squash! WIP
    Merging recent updates

---
+++ docs/precache-manifest.333b3d31efa9523da4eef73cb42ba27c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "520ff0f7302a570b39a9",
+    "url": "/static/css/main.dff0ecc8.chunk.css"
+  },
+  {
+    "revision": "520ff0f7302a570b39a9",
+    "url": "/static/js/main.520ff0f7.chunk.js"
+  },
+  {
+    "revision": "48c1f52f4997aca866be",
+    "url": "/static/js/runtime~main.48c1f52f.js"
+  },
+  {
+    "revision": "478007af6a00cd3246a1",
+    "url": "/static/js/2.478007af.chunk.js"
+  },
+  {
+    "revision": "0e4c47b858ae6f79e735b78831b85dc8",
+    "url": "/static/js/2.478007af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fb3b2ab42548061f10c67380801f6ce",
+    "url": "/index.html"
+  }
+];
